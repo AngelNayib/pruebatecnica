1fcb2eba03068ae9895afcf13e055111a2250e63
Angel Nayib Espinoza Ibañez <50686805+Angel9817@users.noreply.github.com>
2024-08-17 17:28:23 -0400

    add ui

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('producto.index');
 });
+
+Route::group(['middleware' => ['auth:sanctum', 'verified']], function () {});
